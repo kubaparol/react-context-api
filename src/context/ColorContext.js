6c89c72fbaa1f4119fecd3f3a9dae503610241f4
Jakub Parol <kubciop33@gmail.com>
2022-06-12 19:58:17 +0200

    add Color and Text context

---
+++ src/context/ColorContext.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ColorContext = React.createContext('default color');
+
+export default ColorContext;
---
+++ src/context/TextContext.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const TextContext = React.createContext('default text');
+
+export default TextContext;
